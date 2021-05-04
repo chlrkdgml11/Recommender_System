53158151900cf6b3e512197645086fdebfcf0050
choikanghee <chlrkdgml11@naver.com>
2021-05-04 10:22:17 +0900

    Update

--- Find_Distinct_Point/Find_Distinct_Point_By_RMSE.py
+++ Find_Distinct_Point/Find_Distinct_Point_By_RMSE.py
@@ -30,65 +30,69 @@ for i in range(np_test.shape[0]):
 arr = np.zeros((944,2))
 
 for i in range(1,944):
-    if(len(df_train.loc[df_train['user_id'] == i]) >= 20):
+    if((len(df_train.loc[df_train['user_id'] == i]) >= 20) & (len(df_train.loc[df_train['user_id'] == i]) <= 40)):
         arr[i][0] = i
         arr[i][1] = len(df_train.loc[df_train['user_id'] == i])
 
 np.savetxt('./Find_Distinct_Point/over_20_data_users.txt', arr, fmt = '%2d', delimiter = ',', header='test')
 '''
 
-del_user = 68
-rmse_arr = np.zeros((20,2))
+a = np.loadtxt("./Find_Distinct_Point/over_20_data_users.txt", delimiter = ',')
 
-df_only_delected_user = df_train.loc[df_train['user_id'] == del_user]       # 유저 데이터 다 지운 index values 저장
-print(df_only_delected_user.item_id.values)
-print(df_only_delected_user.rating.values)
-df_del_train = df_train.loc[df_train['user_id'] != del_user]      # 유저의 데이터 다 지움
+for i in range(len(a)):
+    del_user = int(a[i][0])
 
 
-np_train = np.array(df_del_train)
-ratings_train = np.zeros((n_users, n_items))
-for i in range(np_train.shape[0]):
-    ratings_train[int(np_train[i][0])-1][int(np_train[i][1])-1] = np_train[i][2]
+    df_only_delected_user = df_train.loc[df_train['user_id'] == del_user]       # 유저 데이터 다 지운 index values 저장
+    df_del_train = df_train.loc[df_train['user_id'] != del_user]      # 유저의 데이터 다 지움
 
 
-for x in range(20):
-    print('x = ', x)
-    print(ratings_train[del_user-1])
-    ratings_train[int(del_user)-1][int(df_only_delected_user.item_id.values[x])-1] = df_only_delected_user.rating.values[x]
+    np_train = np.array(df_del_train)
+    ratings_train = np.zeros((n_users, n_items))
+    for i in range(np_train.shape[0]):
+        ratings_train[int(np_train[i][0])-1][int(np_train[i][1])-1] = np_train[i][2]
+
+    rmse_arr = np.zeros((df_only_delected_user.shape[0],2))
+    print(df_only_delected_user.shape[0])
+    for x in range(df_only_delected_user.shape[0]):
+        print('x = ', x)
+        print(int(del_user), int(df_only_delected_user.item_id.values[x]), df_only_delected_user.rating.values[x])
+        ratings_train[int(del_user)-1][int(df_only_delected_user.item_id.values[x])-1] = df_only_delected_user.rating.values[x]
 
 
-    k=5
-    neigh = NearestNeighbors(n_neighbors=k, metric="cosine")
-    neigh.fit(ratings_train)
+        k=5
+        neigh = NearestNeighbors(n_neighbors=k, metric="cosine")
+        neigh.fit(ratings_train)
 
 
-    top_k_distances, top_k_users = neigh.kneighbors(ratings_train, return_distance=True)
-    user_pred_k = np.zeros(ratings_train.shape)
+        top_k_distances, top_k_users = neigh.kneighbors(ratings_train, return_distance=True)
+        user_pred_k = np.zeros(ratings_train.shape)
 
 
-    for i in range(ratings_train.shape[0]):
-        bottom = np.array([np.abs(top_k_distances[i].T).sum(axis=0)]).T
-        if(bottom[0] == 0):
-            bottom[0] = 1
-        user_pred_k[i, :] = top_k_distances[i].T.dot(ratings_train[top_k_users][i]) / bottom
+        for i in range(ratings_train.shape[0]):
+            bottom = np.array([np.abs(top_k_distances[i].T).sum(axis=0)]).T
+            if(bottom[0] == 0):
+                bottom[0] = 1
+            user_pred_k[i, :] = top_k_distances[i].T.dot(ratings_train[top_k_users][i]) / bottom
 
 
-    sum = 0
-    cnt = 0
-    for i in range(n_users):
-        for j in range(n_items):
-            if((user_pred_k[i][j] != 0) & (ratings_test[i][j] != 0)):
-                sum += (user_pred_k[i][j] - ratings_test[i][j]) ** 2
-                cnt += 1
+        sum = 0
+        cnt = 0
+        for i in range(del_user-1, del_user):
+            for j in range(n_items):
+                if((user_pred_k[i][j] != 0) & (ratings_test[i][j] != 0)):
+                    sum += (user_pred_k[i][j] - ratings_test[i][j]) ** 2
+                    cnt += 1
 
-    if(cnt == 0):
-        rmse = 5
-    else:
-        rmse = sum / cnt
+        if(cnt == 0):
+            rmse = 5
+        else:
+            rmse = sum / cnt
 
-    rmse_arr[x][0] = x
-    rmse_arr[x][1] = np.sqrt(rmse)
+        rmse_arr[x][0] = x
+        rmse_arr[x][1] = np.sqrt(rmse)
 
-print(rmse_arr)
-np.savetxt('./Find_Distinct_Point/rmse.txt', rmse_arr, fmt = '%f', delimiter = ',', header='test')
+    print(rmse_arr)
+    np.savetxt('./Find_Variance_Text/rmse_%d.txt' %del_user, rmse_arr, fmt = '%f', delimiter = '\t', header='test')
+
+
--- Find_Distinct_Point/Find_Distinct_Point_By_Variance.py
+++ Find_Distinct_Point/Find_Distinct_Point_By_Variance.py
@@ -1,21 +1,40 @@
 import numpy as np
-
-data = np.loadtxt("./Find_Distinct_Point/rmse.txt", delimiter = ",", dtype = np.float64)
-
-data = np.array(data)
-
-rmse = []
-
-for i in range(20):
-    rmse.append(data[i][1])
-
-rmse = np.array(rmse)
-print(rmse)
-
-group = []
-
-for i in range(16):
-    group.append(np.var(rmse[i:i+5]))
-    print(round(np.var(rmse[i:i+5]), 8))
+import matplotlib.pyplot as plt
 
 
+over_20_data_users = np.loadtxt("./Find_Distinct_Point/over_20_data_users.txt", delimiter = ",", dtype = np.float64)
+
+
+
+
+
+
+for x in over_20_data_users:
+
+
+    data = np.loadtxt("./Find_Variance_Text/rmse_%d.txt" %int(x[0]), delimiter = "\t", dtype = np.float64)
+
+    data = np.array(data)
+
+    rmse = []
+
+    for i in range(len(data)):
+        rmse.append(data[i][1])
+
+    rmse = np.array(rmse)
+
+    group = []
+
+    plt.figure()
+    plt.title('%d User' %int(x[0]))
+    plt.xlabel('Group')
+    plt.ylabel('Variance')
+    plt.axis([0, 17, -0.05, 1.5])
+    plt.xticks(list(range(16))[::3])
+    for i in range(16):
+        var = round(np.var(rmse[i:i+3]), 6)
+        group.append(var)
+        plt.plot(i+1, var, 'ro')
+
+    # plt.show()
+    plt.savefig('./Find_Variance_Image/%d.png' %int(x[0]))
